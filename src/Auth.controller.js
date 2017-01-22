094b2acc7d4d6fbd3fdee26fb01c90f4fd59375f
Blake Guilloud <fbguillo@gmail.com>
2017-01-22 11:52:55 -0500

    implementing fluyt

--- src/Auth.controller.js
+++ src/Auth.controller.js
@@ -1,17 +1,17 @@
 import { SendSuccess, SendError } from 'tael';
-import Shuttle from './shuttle';
+import Fluyt from 'fluyt';
 import * as UserService from './User.service.js';
 
 export function register(req, res) {
-  return Promise.resolve(Shuttle.liftRequest(req))
-    .then(Shuttle.liftFunction(UserService.register, 'data'))
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(UserService.register, 'data'))
     .then(SendSuccess(res))
     .catch(SendError(res));
 }
 
 export function login(req, res) {
-  return Promise.resolve(Shuttle.liftRequest(req))
-    .then(Shuttle.liftFunction(UserService.getUser, 'requester.userId'))
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(UserService.getUser, 'requester.userId'))
     .then(SendSuccess(res))
     .catch(SendError(res));
 }
